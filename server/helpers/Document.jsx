0b651ce61076eefcfd4958e6693b27d442a7e9e5
Andy Rusterholz <andy@blinker.com>
2016-03-30 23:55:07 -0600

    Load jQuery and Bootstrap JS from CDNs

--- server/helpers/Document.jsx
+++ server/helpers/Document.jsx
@@ -19,6 +19,8 @@ export class Document extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="application-name" content="Nectarine" />
           <link rel="stylesheet" type="text/css" href="/bundle.css" />
+          <script src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossOrigin="anonymous"></script>
         </head>
         <body>
           <div id={contentElement}/>
